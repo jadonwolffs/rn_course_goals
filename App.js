803f62b109ca001aef84a49346d4440888d4bfd6
jadon <jadonwolffs@gmail.com>
2020-03-01 20:41:30 +0200

    modified styling

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.top}>
+        <TextInput placeholder="A goal" style={styles.input} />
+        <Button title="+" style={styles.button} />
+      </View>
+      <View>
+        <Text>Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: "#fff"
+    // alignItems: "center",
+    // justifyContent: "center"
   },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: "90%"
+  },
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+    // width: "10%"
+  },
+  button: { paddingTop: 10 }
 });
